9d35c065c8c9c567f2aa3f0d3d1764ac082afa56
alexander-yuen <79123774+alexander-yuen@users.noreply.github.com>
2021-09-17 22:06:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ Webcam.set({
 
     camera = document.getElementById("camera");
 
-Webcam.attach( '#camera' );
+Webcam.attach("camera");
 
-function take_snapshot(){
+function textsnapshot(){
     Webcam.snap(function(data_uri){
         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
@@ -19,4 +19,23 @@ function take_snapshot(){
 
 console.log('ml5 version', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/scIrxW6uV/',modelLoaded)
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/aKoguyVYp/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+
+function check(){
+    img=document.getElementById("capture_image");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+    }
+}
